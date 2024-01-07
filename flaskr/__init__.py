bbb363ebb48d048388f7a563f960411029e660c1
Mohammad Malek <mhos.malek@gmail.com>
2024-01-07 12:31:05 +0100

    update responses

---
+++ flaskr/__init__.py
@@ -0,0 +1,44 @@
+import os
+
+from flask import Flask
+    
+from .database import db
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        SQLALCHEMY_DATABASE_URI="sqlite:////" + os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+    
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import auth
+    from .check_houses.h2s import holland2stay
+    
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(holland2stay.bp)
+
+    # start the telegram bot
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+        
+
+    return app
---
+++ flaskr/schedulder.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+from .check_houses.h2s.services import fetch_all_cities_results, fetch_city_names_service
+
+
+
+def scheduled_job_to_fetch_city_list_and_send_it_to_email():
+    city_code_dic = fetch_city_names_service()
+    available_to_lottary = None  #enable this to also fetch houses available to lottary
+    results = fetch_all_cities_results(available_to_lottary, city_code_dic)
+    
+    
+schedule.every().day.at("10:30").do(scheduled_job_to_fetch_city_list_and_send_it_to_email)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
