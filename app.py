3e78402baf25fd28ed18d2a4e8cbc62efffb428c
Mohammad Malek <mhos.malek@gmail.com>
2024-01-25 17:36:11 +0100

    deploy project

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from create_app import create_app
 
 app = create_app()
 
+app.route("/")(lambda: "This is a REST API for getting houses data from rental company websites. Docs and more info: https://github.com/MHMALEK/holland-houses-crawler-api")
 
 if __name__ == "__main__":
     app.run()
