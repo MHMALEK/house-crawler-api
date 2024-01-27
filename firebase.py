c37b10cc57e67b3e06eafbc3a3b8c1f3434a4886
Mohammad Malek <mhos.malek@gmail.com>
2024-01-27 22:52:09 +0100

    change json firebase to env variables for deployments

--- firebase.py
+++ firebase.py
@@ -1,18 +1,26 @@
 import os
-import json
-from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials
 
-# Load the environment variables
-load_dotenv()
 
-# Get the service account key from the environment variables
-service_account_key = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
-
-# Convert the string back into a JSON object
-service_account_info = json.loads(service_account_key)
-
-# Use the service account info to initialize the Firebase admin SDK
-cred = credentials.Certificate(service_account_info)
-firebase_admin.initialize_app(cred)
+def init_firebase():
+    cred = credentials.Certificate(
+        {
+            "type": os.getenv("TYPE"),
+            "project_id": os.getenv("PROJECT_ID"),
+            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+            "private_key": os.getenv("PRIVATE_KEY"),
+            "client_email": os.getenv("CLIENT_EMAIL"),
+            "client_id": os.getenv("CLIENT_ID"),
+            "auth_uri": os.getenv("AUTH_URI"),
+            "token_uri": os.getenv("TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+        }
+    )
+    firebase_admin.initialize_app(
+        cred,
+        {
+            "databaseURL": "https://house-crawler-api-2851f-default-rtdb.europe-west1.firebasedatabase.app"
+        },
+    )
