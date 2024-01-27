e47b7d50fa7cae4f7892f7d8a79950dd4a4734d4
Mohammad Malek <mhos.malek@gmail.com>
2024-01-28 00:30:22 +0100

    update auth

--- auth.py
+++ auth.py
@@ -11,22 +11,38 @@ bp = Blueprint("auth", __name__, url_prefix="/users")
 @bp.route("/register", methods=["POST"])
 def register():
     chat_id = request.json.get("chat_id")
-    ref = db.reference('chat_ids')
-    ref.child(chat_id).set(True)
+    ref = db.reference("chat_ids")
+    ref.child(str(chat_id)).set(True)
     return {"chat_id": chat_id}, 201
 
 
 @bp.route("/list", methods=["GET"])
 def list_users():
-    ref = db.reference('chat_ids')
+    ref = db.reference("chat_ids")
     chat_ids = ref.get()
-    return {"chat_ids": list(chat_ids.keys())}
+    if chat_ids is None:
+        return {"chat_ids": []}
+    else:
+        return {"chat_ids": list(chat_ids.keys())}
+
+
+@bp.route("/<chat_id>", methods=["GET"])
+def get_single_user(chat_id):
+    if chat_id is None:
+        return jsonify({"error": "Missing chat_id parameter"}), 400
+    ref = db.reference("chat_ids")
+    user = ref.child(chat_id).get()
+
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+
+    return jsonify({"chat_id": chat_id}), 200
 
 
 @bp.route("/<chat_id>", methods=["DELETE"])
 def remove_user(chat_id):
     chat_id_to_remove = chat_id
-    ref = db.reference('chat_ids')
+    ref = db.reference("chat_ids")
     ref.child(chat_id_to_remove).delete()
     return {"removed_chat_id": chat_id_to_remove}, 200
 
