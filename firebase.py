0a6236c5e3dbd177ef09f2fc0c8060fcbc0f308c
Mohammad Malek <mhos.malek@gmail.com>
2024-01-27 22:33:22 +0100

    add env for deploy

--- firebase.py
+++ firebase.py
@@ -1,11 +1,18 @@
+import os
+import json
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials
 
+# Load the environment variables
+load_dotenv()
 
-def init_firebase():
-    cred = credentials.Certificate(
-        "./house-crawler-api-2851f-firebase-adminsdk-gd2sx-aae4c937b8.json"
-    )
-    firebase_admin.initialize_app(cred, {
-        'databaseURL' : 'https://house-crawler-api-2851f-default-rtdb.europe-west1.firebasedatabase.app'
-    })
+# Get the service account key from the environment variables
+service_account_key = os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
+
+# Convert the string back into a JSON object
+service_account_info = json.loads(service_account_key)
+
+# Use the service account info to initialize the Firebase admin SDK
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred)
