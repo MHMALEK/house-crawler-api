3e9fe5abcdc7dc7ee7fb5e0fc44348fccc990f25
Mohammad Malek <mhos.malek@gmail.com>
2024-01-27 23:23:08 +0100

    debug

--- firebase.py
+++ firebase.py
@@ -4,13 +4,12 @@ from firebase_admin import credentials
 
 
 def init_firebase():
-    print("malek:", os.getenv("PRIVATE_KEY"))
     cred = credentials.Certificate(
         {
             "type": os.getenv("TYPE"),
             "project_id": os.getenv("PROJECT_ID"),
             "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-            "private_key": os.getenv("PRIVATE_KEY"),
+            "private_key": os.getenv("PRIVATE_KEY").replace('\\n', ''),
             "client_email": os.getenv("CLIENT_EMAIL"),
             "client_id": os.getenv("CLIENT_ID"),
             "auth_uri": os.getenv("AUTH_URI"),
